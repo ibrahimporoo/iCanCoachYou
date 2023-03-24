3e352800c6ad56079081f18f526beeaa9d2042da
Ibrahim Abdo <ibrahimporoo@gmail.com>
2023-03-24 17:43:56 +0200

    Uploading the first version

---
+++ assets/js/index.js
@@ -0,0 +1,241 @@
+// import { initializeApp } from 'firebase/app'
+// import {
+// 	getFirestore, collection, getDocs
+// } from 'firebase/firestore/lite';
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBsBaihwh8F_UY8oYEsfcMlQEwEIgXcbxc",
+//   authDomain: "elmawkaabeta.firebaseapp.com",
+//   databaseURL: "https://elmawkaabeta.firebaseio.com",
+//   projectId: "elmawkaabeta",
+//   storageBucket: "elmawkaabeta.appspot.com",
+//   messagingSenderId: "808588970288",
+//   appId: "1:808588970288:web:8fe9fcbf5e7ca8cca820f5",
+//   measurementId: "G-G8FTTQ0EB2"
+// };
+
+// initializeApp(firebaseConfig)
+// const db = getFirestore()
+
+// const lang = document.querySelector('html').lang; // get page's Lang to assign it to database
+
+// const colRef = collection(db, 'coaches', 'languages', lang);
+
+// // selecting the coaches Row in html file
+// const coachesContent = document.getElementById('coaches-content');
+// let coaches = []; // for fulling coaches in coaches page
+// let html = ''; // content that we put in html
+// // let html_filtered_coaches = ''; // content that we put in html
+// coachesContent.innerHTML = ''; // empty coaches content before getting data
+
+// async function getData() {
+// 	// Fetching 'Getting' Data
+// 	await getDocs(colRef)
+// 	.then((snapshot) => {
+// 		// Check if we in the home page or top coaches page
+// 		snapshot.docs.forEach((doc) => {
+// 			coaches.push({ ...doc.data(), id: doc.id });
+// 		})
+// 		coaches.sort((a, b) => a.order - b.order); // sort the array according to it's order
+// 		// in the index 'top coaches' page
+// 		if(coachesContent.classList.contains('top-coaches')) {
+// 			// Adding Content of Data coming from Firebase to HTML
+// 			coaches = coaches.filter(coach => coach.order <= 3)
+// 		}
+// 		coaches.map(coach => {
+// 			html += `
+// 			<div class="col-lg-4 col-md-6">
+// 				<div class="member" data-aos="zoom-in">
+// 					<div class="pic"><img src="${coach.image}" class="img-fluid" alt="Coach Image"></div>
+// 						<div class="member-info coaches pricing">
+// 							<div class='ps-3 pe-3'>
+// 								<h5>${coach.name}</h5>
+// 								<h4>${coach.jobTitle}</h4>
+// 							</div>
+// 							<span>${coach.pricing}</span>
+// 							<p class='detail-item mb-1 mt-1'>Details</p>
+// 							<span>${coach.category}</span>
+// 							<span>${coach.summary.length > 180 ? coach.summary.slice(0, 180) : coach.summary}</span>
+// 							<span>${coach.country}/${coach.city} - ${coach.rating} stars</span>
+// 							<div class="social">
+// 								<a href="${coach.SM_account}" target="_blank"><i class="bi bi-linkedin"></i></a>
+// 							</div>
+// 							<a href="${coach.paymentLink}" target="_blank" class="btn-buy mt-2">Buy Now</a>
+// 						</div>
+// 					</div>
+// 				</div>
+// 			</div>
+// 		`;
+// 		})
+// 	})
+// 	.catch( _ => {
+// 		coachesContent.innerHTML = `<h1 style='padding: 100px 0; text-align: center'>No coaches till now</h1>`;
+// 	});
+// };
+
+// getData().then(() => {
+// 	if(coachesContent.classList.contains('top-coaches')) {
+// 		coachesContent.innerHTML = html;
+// 	} else if (coachesContent.classList.contains('filtered-coaches')) {
+// 		coachesContent.innerHTML = html;
+// 	} else {
+// 		coachesContent.innerHTML = `<h1 style='padding: 100px 0; text-align: center'>No coaches till now</h1>`;
+// 	};
+// });
+
+// function setSummary(summary) {
+// 	console.log(summary);
+// }
+
+/* Hiding Filtered Boxes with select box */
+// const inputFields = document.querySelectorAll('.input-field');
+
+// // if click outside
+// window.onclick = (e) => {
+// 	if(!e.target.classList.contains('filter-el')) {
+// 		inputFields.forEach(field => field.classList.remove('on'));
+// 	}
+// };
+
+// inputFields.forEach(field => {
+// 	field.addEventListener('click', () => {
+// 		field.classList.toggle('on');
+// 	})
+// });
+
+// // Select Dropdown menues
+// const byIndustry = document.querySelectorAll('.dropdown.by-industry li');
+// const byCountry = document.querySelectorAll('.dropdown.by-country li');
+// const byJobTitle = document.querySelectorAll('.dropdown.by-job-title li');
+// // adding checked to the first item in the list
+// checkTheElement(byIndustry, 'category');
+// checkTheElement(byCountry, 'country');
+// checkTheElement(byJobTitle, 'jobTitle');
+
+// function checkTheElement(el, filterBy) {
+
+// 	el.forEach(item => {
+
+// 		item.addEventListener('click', () => {
+
+// 			el.forEach(item => {
+// 				if(item.classList.contains('on')) {
+// 					item.classList.remove('on');
+// 					item.querySelector('i').classList.remove('on');
+// 				}
+// 			});
+			
+// 			item.classList.add('on');
+// 			item.querySelector('i').classList.add('on');
+			
+// 			// Search the Item
+// 			let searchingVal = item.dataset.category;
+// 			if(searchingVal != 'all') {
+// 				switch(filterBy) {
+// 					case 'category':
+// 						filteredCoaches = coaches.filter( coach => {
+// 							if(coach.category.toLowerCase().indexOf(searchingVal) != -1) {
+// 								return coach;
+// 							}
+// 						})
+// 						showNewCoaches(filteredCoaches);
+// 						break;
+// 					case 'country':
+// 						filteredCoaches = coaches.filter( coach => {
+// 							if(coach.country.toLowerCase().indexOf(searchingVal) != -1) {
+// 								return coach;
+// 							}
+// 						})
+// 						showNewCoaches(filteredCoaches);
+// 						break;
+// 					case 'jobTitle':
+// 						filteredCoaches = coaches.filter( coach => {
+// 							if(coach.jobTitle.toLowerCase().indexOf(searchingVal) != -1) {
+// 								return coach;
+// 							}
+// 						})
+// 						showNewCoaches(filteredCoaches);
+// 						break;
+// 					default:
+// 						return coaches;
+// 				}
+// 			} else {
+// 				showNewCoaches(coaches);
+// 			}
+// 		})
+// 	});
+
+// };
+
+// Handle Events of searching and filtering when inputs changes
+
+// let searchField = document.getElementById('search-field');
+// let filteredCoaches = coaches;
+// let html_filtering_by_user = '';
+
+// Handle User searching
+// searchField.addEventListener('keyup', () => {
+// 	let searchFieldValue = searchField.value.toLowerCase();
+// 	if(searchFieldValue !== '') {
+// 		filteredCoaches = coaches.filter( coach => {
+// 			if(
+// 				coach.category.toLowerCase().indexOf(searchFieldValue) != -1 ||
+// 				coach.name.toLowerCase().indexOf(searchFieldValue) != -1 ||
+// 				coach.jobTitle.toLowerCase().indexOf(searchFieldValue) != -1 ||
+// 				coach.country.toLowerCase().indexOf(searchFieldValue) != -1
+// 			) {
+// 				return coach;
+// 			}
+// 		})
+// 		showNewCoaches(filteredCoaches);
+// 	} else {
+// 		return false
+// 	};
+// })
+// Handle User Choosed Categories
+
+// function showNewCoaches(coaches) {
+// 	html_filtering_by_user = '';
+// 	coaches.map(coach => {
+// 		html_filtering_by_user += `
+// 		<div class="col-lg-4 col-md-6">
+// 			<div class="member" data-aos="zoom-in">
+// 				<div class="pic"><img src="${coach.image}" class="img-fluid" alt="Coach Image"></div>
+// 					<div class="member-info coaches pricing">
+// 						<div class='ps-3 pe-3'>
+// 							<h5>${coach.name}</h5>
+// 							<h4>${coach.jobTitle}</h4>
+// 						</div>
+// 						<span>${coach.pricing}</span>
+// 						<p class='detail-item mb-1 mt-1'>Details</p>
+// 						<span>${coach.category}</span>
+// 						<span>${coach.summary}</span>
+// 						<span>${coach.country}/${coach.city} - ${coach.rating} stars</span>
+// 						<div class="social">
+// 							<a href="${coach.SM_account}" target="_blank"><i class="bi bi-linkedin"></i></a>
+// 						</div>
+// 						<a href="${coach.paymentLink}" target="_blank" class="btn-buy mt-2">Buy Now</a>
+// 					</div>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	`;
+// 	})
+// 	if(coaches.length > 0) {
+// 		coachesContent.innerHTML = html_filtering_by_user;
+// 	} else {
+// 		if(lang == 'ar') {
+// 			coachesContent.innerHTML = `
+// 				<h1 style='padding: 100px 0; text-align: center'>لا يوجد مدربون علي حسب اختيارك</h1>
+// 			`;
+// 		} else {
+// 			coachesContent.innerHTML = `
+// 			<h1 style='padding: 100px 0; text-align: center'>There are no Coaches according to your filtration</h1>
+// 			`;
+// 		}
+// 	}
+// };
+
+
+
---
+++ assets/js/multiple-steps-form.js
@@ -0,0 +1,183 @@
+
+
+// Popup Multiple Steps Form Menu
+// Multi Step Form - Joining
+const joinForm = document.querySelector('.join-form');
+const joinUs = document.querySelector('.join-us');
+const closeFormBtn = document.querySelector('.close-form');
+document.querySelector('.join-us').addEventListener('click', _ => {
+	joinForm.classList.toggle('on');
+	document.body.classList.toggle('hide-flow');
+})
+closeFormBtn.addEventListener('click', (e) => {
+	joinForm.classList.toggle('on');
+	document.body.classList.toggle('hide-flow');
+	console.log(e.target)
+})
+// Multiple Steps Form
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const taps = document.querySelectorAll('.fields .step');
+const progSteps = document.querySelectorAll('.steps h4');
+let tapIndex = 2;
+showTap(tapIndex);
+
+
+function formSubmit() {
+	console.log("submit");
+	document.querySelector('.steps').style.cssText = 'display: none';
+	document.querySelector('.fields').classList.add('done');
+	document.querySelector('.form-submitted').classList.add('done');
+}
+
+nextBtn.addEventListener('click', () => {
+	prevNext(1);
+	// stepIndex += 1;
+	// stepIndex = stepIndex >= steps.length ? 0 : steps.length - 1;
+	// showStep(stepIndex);
+});
+prevBtn.addEventListener('click', () => {
+	prevNext(-1);
+	// stepIndex -= 1;
+	// stepIndex = stepIndex < 0 ? steps.length - 1 : 0;
+	// showStep(stepIndex);
+});
+
+function showTap(tapIndex) {
+	taps.forEach(tap => {
+		tap.classList.remove('on');
+	})
+	// Show Current Tap
+	taps[tapIndex].classList.add('on');
+	if(tapIndex > 0) {
+		// Show User Progress
+		progSteps.forEach((prog, index) => {
+			if(index < tapIndex) {
+				prog.classList.add('on');
+			} else {
+				prog.classList.remove('on');
+			}
+		})
+		// Show Previous Btn
+		prevBtn.style.display = 'block'
+	} else {
+		// Show That is no progress yet
+		progSteps.forEach(prog => {
+			prog.classList.remove('on');
+		})
+		// Hide Previous Btn
+		prevBtn.style.display = 'none'
+	}
+	// Make Next Btn Submit if it's the last tap
+	if(tapIndex == taps.length - 1) {
+		nextBtn.innerHTML = "Submit"
+	} else {
+		nextBtn.innerHTML = "Next";
+	}
+};
+
+function prevNext(n) {
+	// Check if current validations' inputs are true
+	if (n == 1 && isValid()) {
+		// Go To Next Tab
+		tapIndex += n;
+		// If the last submit Form
+		if (tapIndex >= taps.length) {
+			formSubmit();
+			tapIndex = taps.length - 1;
+			return false;
+		}
+		showTap(tapIndex);
+	} else if (n == -1) { // If Previous Btn clicked
+		// Go To Previous Tab
+		tapIndex += n;
+		showTap(tapIndex);
+	} else {
+		return false;
+	}
+}
+
+// Alert
+const sweetAlert = document.querySelector('#alert-container');
+const closeAlert = document.querySelector('#alert-container #close-alert');
+closeAlert.addEventListener('click', () => {
+	sweetAlert.classList.remove('on');
+});
+sweetAlert.addEventListener('click', () => {
+	sweetAlert.classList.remove('on');
+})
+function isValid() {
+	let valid = true;
+	let currentStep = document.querySelector('.fields .step.on');
+	let currentInputs = currentStep.getElementsByTagName('input');
+	for(let i = 0;i < currentInputs.length; i++) {
+		// If There is any empty Input make it invalid
+		if (currentInputs[i].value.length) {
+			currentInputs[i].classList.remove("invalid");
+		} else {
+			valid = false;
+			currentInputs[i].classList.add("invalid");
+		}
+	}
+	if(tapIndex == 0) {
+		let mailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+		console.log(currentInputs[8])
+		if(currentInputs[8].value.match(mailPattern)) {
+			currentInputs[8].classList.remove('invalid');
+			valid = true;
+		} else {
+			currentInputs[8].classList.add('invalid');
+			valid = false;
+		}
+	}
+	if(tapIndex == 1) {
+		let validOne, validTwo, validThree;
+		textareas = currentStep.querySelectorAll('textarea');
+		industryboxes = currentStep.querySelectorAll('input[name="industry"]:checked');
+		jobTitleboxes = currentStep.querySelectorAll('input[name="jobTitle"]:checked');
+		// Text Area
+		for(let i = 0;i < textareas.length; i++) {
+			// If There are any empty Input make it invalid
+			if (textareas[i].value.length) {
+				textareas[i].classList.remove("invalid");
+				validOne = true
+			} else {
+				validOne = false;
+				textareas[i].classList.add("invalid");
+			}
+		};
+		// Industry and job title Boxes
+		if(industryboxes.length == 0 || jobTitleboxes.length == 0) {
+			validTwo = false;
+		} else {
+			validTwo = true;
+		}
+		// Linked In Profile URL
+		// let linkedInRegex = /^(http(s)?:\/\/)?([\w]+\.)?linkedin\.com\/(pub|in|profile)/;
+		// let userLink = document.querySelector('input[name="linkedIn_link"]');
+		// if(!userLink.value.match(linkedInRegex)) {
+		// 	validThree = false;
+		// 	userLink.classList.add('invalid')
+		// } else {
+		// 	validThree = true;
+		// 	userLink.classList.remove('invalid')
+		// }
+		// Finally Validation
+		if(validOne == true && validTwo == true) {
+			valid = true;
+		} else {
+			valid = false;
+		}
+		// else {
+		// 	if((validOne == false && validTwo == false && validThree == true) || (validOne == false && validTwo == false && validThree == false)) {
+		// 		sweetAlert.classList.add('on');
+		// 	}
+		// 	if(validOne == true && validTwo == true && validThree == false) {
+		// 		sweetAlert.querySelector('.alert-title').innerText = 'Note That Linked In profile Link is Invalid!';
+		// 		sweetAlert.classList.add('on');
+		// 		console.log("Linked In Profile Checker")
+		// 	}
+		// }
+	}
+	return valid;
+}
